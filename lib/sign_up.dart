f20ee0849c85fbb41a8070da1d5c255d518357a3
muyaho-universe <kimdaesk@gmail.com>
2022-09-26 17:41:54 +0900

    Complete sign_up.dart style

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -21,6 +21,7 @@ class _SignUpPageState extends State<SignUpPage> {
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
           children: [
+            const SizedBox(height: 12.0),
             TextField(
               controller: _usernameController,
               decoration: const InputDecoration(
@@ -30,6 +31,50 @@ class _SignUpPageState extends State<SignUpPage> {
             ),
 // spacer
             const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordConfirmController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Confirm Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _emailAddressController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Email Address',
+              ),
+            ),
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              children: <Widget>[
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xFFE0E0E0),
+                  ),
+                  child: const Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ],
         ),
       ),
