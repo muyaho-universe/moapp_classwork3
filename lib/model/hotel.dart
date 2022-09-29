eac9153ee38484d15b85d6cd4d17591118c9dfcf
muyaho-universe <kimdaesk@gmail.com>
2022-09-30 00:49:23 +0900

    Create hotel.dart

---
+++ lib/model/hotel.dart
@@ -0,0 +1,14 @@
+class Hotel{
+
+  Hotel({
+    required this.loacation,
+    required this.isFavorited,
+    required this.name,
+    required this.rate,
+  });
+
+  int rate;
+  String loacation;
+  bool isFavorited;
+  String name;
+}
