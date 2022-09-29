e12870d7a6120399428aa48e008166ee29e858cf
muyaho-universe <kimdaesk@gmail.com>
2022-09-29 18:33:19 +0900

    Update home_list.dart

--- lib/home.dart
+++ lib/home.dart
@@ -14,6 +14,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shrine/home_list.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'model/product.dart';
@@ -32,64 +33,6 @@ class _HomePageState extends State<HomePage> {
 
 
   bool _isList = false;
-
-  // TODO: Make a collection of cards (102)
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-
-    if (products.isEmpty) {
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString());
-
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        // TODO: Adjust card heights (103)
-        child: Column(
-          // TODO: Center items on the card (103)
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18 / 11,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-                // TODO: Adjust the box size (102)
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                child: Column(
-                  // TODO: Align labels to the bottom and center (103)
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  // TODO: Change innermost Column (103)
-                  children: <Widget>[
-                    // TODO: Handle overflowing labels (103)
-                    Text(
-                      product.name,
-                      style: theme.textTheme.headline6,
-                      maxLines: 1,
-                    ),
-                    const SizedBox(height: 8.0),
-                    Text(
-                      formatter.format(product.price),
-                      style: theme.textTheme.subtitle2,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
-  }
 
   // TODO: Add a variable for Category (104)
   @override
@@ -234,17 +177,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
           ),
-          new Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
-              padding: const EdgeInsets.all(16.0),
-              childAspectRatio: 8.0 / 9.0,
-              shrinkWrap: true,
-              // TODO: Build a grid of cards (102)
-              children: _buildGridCards(context),
-              physics: BouncingScrollPhysics(),
-            ),
-          ),
+          // HomeListPage(_isList),
         ],
       ),
     );
@@ -254,19 +187,5 @@ class _HomePageState extends State<HomePage> {
     if (!await launchUrl(_url)) {
       throw 'Could not launch $_url';
     }
-  }
-}
-
-class ChangeWidget extends StatefulWidget {
-  const ChangeWidget({Key? key}) : super(key: key);
-
-  @override
-  State<ChangeWidget> createState() => _ChangeWidgetState();
-}
-
-class _ChangeWidgetState extends State<ChangeWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
