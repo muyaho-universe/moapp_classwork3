f0e77310a664e99267c088a757ec23896af1c89c
muyaho-universe <kimdaesk@gmail.com>
2022-10-02 01:39:51 +0900

    Update hotel description and phone number

--- lib/model/hotel.dart
+++ lib/model/hotel.dart
@@ -1,6 +1,7 @@
-class Hotel{
-
+class Hotel {
   const Hotel({
+    required this.phoneNumber,
+    required this.description,
     required this.location,
     required this.name,
     required this.rate,
@@ -9,8 +10,12 @@ class Hotel{
 
   final int rate;
   final String location;
+
   // late bool isFavorited;
   final String name;
+  final String phoneNumber;
+  final String description;
   final int id;
+
   String get assetName => 'hotel0$id.jpg';
 }
