500c278e0b72c756cee378db77e3d73768193dd5
muyaho-universe <kimdaesk@gmail.com>
2022-10-02 12:32:36 +0900

    Working on double tap

--- lib/detail.dart
+++ lib/detail.dart
@@ -13,6 +13,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  bool isFavorited = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,57 +24,122 @@ class _DetailPageState extends State<DetailPage> {
       ),
       body: ListView(
         // crossAxisAlignment: CrossAxisAlignment.center,
-        children:<Widget> [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 3,
-            child: Hero(
-              tag: widget.hotel.assetName,
-              child: Image.asset(
-                widget.hotel.assetName,
-                fit: BoxFit.fill,
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 3,
+                child: Hero(
+                  tag: widget.hotel.assetName,
+                  child: Image.asset(
+                    widget.hotel.assetName,
+                    fit: BoxFit.fill,
+                  ),
+                ),
               ),
-            ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
           ),
           Padding(
             padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 4.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget> [
+              children: <Widget>[
                 Row(
                   children: starRate(widget.hotel.rate),
                 ),
                 SizedBox(
-                  height: 10,
+                  height: 15,
                 ),
                 Text(widget.hotel.name),
                 SizedBox(
-                  width: 100,
-                  child: DefaultTextStyle(
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      // fontFamily: 'Horizon',
-                    ),
-                    child: AnimatedTextKit(
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                          'Hello world!',
-                          textStyle: const TextStyle(
-                            fontSize: 32.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          speed: const Duration(milliseconds: 2000),
-                        ),
-                      ],
-
-                      totalRepeatCount: 4,
-                      pause: const Duration(milliseconds: 1000),
-                      displayFullTextOnTap: true,
-                      stopPauseOnTap: true,
-                    )
-                  ),
+                  height: 15,
                 ),
-
+                Row(
+                  children: [
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.lightBlue,
+                    ),
+                    Text(
+                      widget.hotel.location,
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.lightBlue,
+                    ),
+                    Text(
+                      widget.hotel.phoneNumber,
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                Text(
+                  widget.hotel.description,
+                  maxLines: 100,
+                  style: TextStyle(
+                    color: Colors.lightBlue,
+                  ),
+                )
+                // SizedBox(
+                //   width: 100,
+                //   child: DefaultTextStyle(
+                //     style: TextStyle(
+                //       fontSize: 40.0,
+                //       // fontFamily: 'Horizon',
+                //     ),
+                //     child: AnimatedTextKit(
+                //       animatedTexts: [
+                //         TypewriterAnimatedText(
+                //           'Hello world!',
+                //           textStyle: const TextStyle(
+                //             fontSize: 32.0,
+                //             fontWeight: FontWeight.bold,
+                //           ),
+                //           speed: const Duration(milliseconds: 2000),
+                //         ),
+                //       ],
+                //
+                //       totalRepeatCount: 4,
+                //       pause: const Duration(milliseconds: 1000),
+                //       displayFullTextOnTap: true,
+                //       stopPauseOnTap: true,
+                //     )
+                //   ),
+                // ),
               ],
             ),
           ),
