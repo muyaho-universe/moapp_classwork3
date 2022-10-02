4552163f861bc8536ab620efac477695928a899c
muyaho-universe <kimdaesk@gmail.com>
2022-10-02 11:47:28 +0900

    Working on AnimationKit

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,11 +1,18 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shrine/model/hotel.dart';
 
 //TODO: Hero Animation
-class DetailPage extends StatelessWidget {
+class DetailPage extends StatefulWidget {
   DetailPage(this.hotel, {Key? key}) : super(key: key);
   final Hotel hotel;
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +21,59 @@ class DetailPage extends StatelessWidget {
         title: Text("Detail"),
         centerTitle: true,
       ),
-      body: Column(
+      body: ListView(
         // crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+        children:<Widget> [
           SizedBox(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height / 3,
             child: Hero(
-              tag: hotel.assetName,
+              tag: widget.hotel.assetName,
               child: Image.asset(
-                hotel.assetName,
+                widget.hotel.assetName,
                 fit: BoxFit.fill,
               ),
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(8.0),
+            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 4.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(hotel.name),
+              children: <Widget> [
+                Row(
+                  children: starRate(widget.hotel.rate),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(widget.hotel.name),
+                SizedBox(
+                  width: 100,
+                  child: DefaultTextStyle(
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      // fontFamily: 'Horizon',
+                    ),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText(
+                          'Hello world!',
+                          textStyle: const TextStyle(
+                            fontSize: 32.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          speed: const Duration(milliseconds: 2000),
+                        ),
+                      ],
+
+                      totalRepeatCount: 4,
+                      pause: const Duration(milliseconds: 1000),
+                      displayFullTextOnTap: true,
+                      stopPauseOnTap: true,
+                    )
+                  ),
+                ),
+
               ],
             ),
           ),
